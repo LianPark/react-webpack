8db24957086fe6398ebd369c63ada294b68459e6
Lian Park <woori01@gmail.com>
2024-07-27 15:13:24 +0900

    readme 파일수정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-webpack-babel
+
+웹팩 + 바벨을 이용하여 리액트 사용해보기
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello, React with Webpack + Babel!</h1>
+      <p>리액트를 웹팩 + 바벨을 이용하여 사용해본다.</p>
     </div>
   );
 };
